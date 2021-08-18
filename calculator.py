c85aed5865bcd489ec81053a7673f303c205f5a3
Mike <michael.valenciav@gmail.com>
2021-08-18 16:04:42 -0500

    refactoring calculator function

--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
 def add(numbers: str) -> int:
     if numbers:
         if ',' in numbers:
-            x = numbers.split(',')
-            tmp = 0
-            for i in x:
-                tmp = tmp + int(i)
-            return tmp
+            int_numbers = [int(x) for x in numbers.split(",")]
+
+            sum_value = 0
+            for value in int_numbers:
+                sum_value = sum_value + value
+            
+            return sum_value
+        
         return int(numbers)
 
     return 0
