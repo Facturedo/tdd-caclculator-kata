e274ffdd2cec6001a8b3ec195075db45dbe692dd
Mike <michael.valenciav@gmail.com>
2021-08-18 15:22:17 -0500

    calculator function returns 0 on empty string.

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from calculator import add
 
 
+
 def test_zero_for_empty_strings():
-    """If function is calle with an empty string, the returned value should be zero."""
+    """If function is called with an empty string, the returned value should be zero."""
     assert add("") == 0
